8b491972f433e1f98dafaedb0ce06046ef8116cc
Henry <hspradley91@gmail.com>
2022-01-20 21:56:50 -0600

    Added settings file and refactored cftw.py

--- chaos_from_the_warp.py
+++ chaos_from_the_warp.py
@@ -6,8 +6,9 @@ Created on Thu Jan 20 21:25:44 2022
 """
 
 import sys
-
 import pygame
+
+from settings import Settings
 
 class WarpInvasion:
     """General class that manages game assets and behavior"""
@@ -15,17 +16,20 @@ class WarpInvasion:
     def __init__(self):
         """Initialize the game and allocate game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Chaos from the Warp")
-        
+
     def run_game(self):
         """Main loop for the game"""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
-            pygame.display.flip()
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip() # Make the screen visible
             
 if __name__ == '__main__':
     # Make a game instance and run the game
---
+++ settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 21:48:47 2022
+
+@author: Hank
+"""
+
+class Settings:
+    """A class for storing all game settings"""
+    
+    def __init__(self):
+        """Initialize the game settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (25, 25, 25)
